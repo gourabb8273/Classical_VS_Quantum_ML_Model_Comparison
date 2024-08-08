e5a61d3c57e4c8684cf9398d7f355588da82efb7
Gourab Banerjee <gourab.banerjee@coxautoinc.com>
2024-08-08 16:16:48 +0530

    adding data

--- app.py
+++ app.py
@@ -693,5 +693,5 @@ def plot():
     plt.close('all')
     # Return the image as a response
     return send_file(img, mimetype='image/png')
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug=True)
+# if __name__ == '__main__':
+#     app.run(host="0.0.0.0", debug=True)
